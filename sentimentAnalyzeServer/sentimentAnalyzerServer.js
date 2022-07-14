f187563a7f3a41bc926e78f481544adba0935767
spranav1998 <s.pranav1998@gmail.com>
2022-07-14 10:01:47 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -26,7 +26,7 @@ function getNLUInstance() {
     const { IamAuthenticator } = require('ibm-watson/auth');
 
     const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-        version: '2021-08-01',
+        version: '2022-04-07',
         authenticator: new IamAuthenticator ({
             apikey: api_key
         }),
